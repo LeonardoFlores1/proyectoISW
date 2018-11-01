9c18fd596a3ef685efe36069d3b975a35dc6276c
Luis Antonio Maldonado <LuiisM22@hotmail.com>
2018-11-01 12:37:39 -0600

    Delete coneccion.php

--- coneccion.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$usuario="root";
-	$pass	="";
-	$servidor="localhost";
-	$coneccion=mysqli_connect($servidor,$usuario,$pass)or die('No se puede conectar al servidor');
-	mysqli_select_db($coneccion,'isw');
-	mysqli_set_charset($coneccion, "utf8"); 
-?>
